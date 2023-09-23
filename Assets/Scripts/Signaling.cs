1b3384374a77d799aa5d349eb29a0476c6f503d2
zaaahrar <zaahrar@mail.ru>
2023-09-23 15:13:15 +0300

    commit2
    ds

--- Assets/Scripts/Signaling.cs
+++ Assets/Scripts/Signaling.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(SpriteRenderer))]
+[RequireComponent(typeof(AudioSource))]
 public class Signaling : MonoBehaviour
 {
     private AudioSource _audioSource;
